cc0714010d127c8b0910df797c95beef95d22238
Rafael Gottardi <rafael.gottardi@gmail.com>
2024-08-14 11:23:48 -0300

    Improvments

--- killercoda.sh
+++ killercoda.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+# Install dependencies
+sudo apt-get update -y
+sudo apt-get install -y git python3 python3-pip
+
+# Clone repo
 git clone https://github.com/RafaelFino/learn-ops-content.git
 cd learn-ops-content
 
-./prepare.sh
+# Install Python packages
+pip3 install -r requirements.txt
 
+# Run the application
 cd src
 python3 start.py
 
